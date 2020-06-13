b60d39269ed4ab992f82ddab21715888a1e01e63
ivopecce <ivo.pecce@gmail.com>
2020-06-13 08:34:36 +0200

    .

---
+++ src/main/java/it/univaq/disim/oop/joblink/controller/LayoutController.java
@@ -0,0 +1,62 @@
+package it.univaq.disim.oop.joblink.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.univaq.disim.oop.joblink.view.ViewDispatcher;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Separator;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+
+public class LayoutController implements Initializable {
+
+	private static final MenuElement MENU_HOME = new MenuElement("Home", "home");
+
+	private static final MenuElement[] MENU_AZIENDA = { new MenuElement("Gestione Offerte di lavoro", "offerte"),
+			new MenuElement("Ricerca utenti", "piani"), new MenuElement("Candidature", "candidature"), new MenuElement("Chat", "chat") };
+	@FXML
+	private VBox menuBar;
+	
+	private ViewDispatcher dispatcher;
+	
+	public LayoutController() {
+		dispatcher = ViewDispatcher.getInstance();
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		menuBar.getChildren().addAll(createButton(MENU_HOME));
+		menuBar.getChildren().add(new Separator());
+		for (MenuElement menu : MENU_AZIENDA) {
+			menuBar.getChildren().add(createButton(menu));
+		}
+	}
+
+	@FXML
+	public void esciAction(MouseEvent event) {
+		dispatcher.logout();
+		
+	}
+
+	private Button createButton(MenuElement viewItem) {
+		Button button = new Button(viewItem.getNome());
+		button.setStyle("-fx-background-color: transparent; -fx-font-size: 14;");
+		button.setTextFill(Paint.valueOf("white"));
+		button.setPrefHeight(10);
+		button.setPrefWidth(180);
+		button.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				dispatcher.renderView(viewItem.getVista());
+			}
+		});
+		return button;
+	}
+
+}
---
+++ src/main/java/it/univaq/disim/oop/joblink/controller/MenuElement.java
@@ -0,0 +1,30 @@
+package it.univaq.disim.oop.joblink.controller;
+
+public class MenuElement {
+
+	private String nome;
+	private String vista;
+
+	public MenuElement(String nome, String vista) {
+		super();
+		this.nome = nome;
+		this.vista = vista;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getVista() {
+		return vista;
+	}
+
+	public void setVista(String vista) {
+		this.vista = vista;
+	}
+
+}
