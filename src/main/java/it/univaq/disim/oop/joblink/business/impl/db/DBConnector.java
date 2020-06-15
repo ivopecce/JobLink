044a19e360b43f7db833431414975e7aa3f9a21b
ivopecce <ivo.pecce@gmail.com>
2020-06-15 19:34:18 +0200

    connessione database

---
+++ src/main/java/it/univaq/disim/oop/joblink/business/impl/db/DBConnector.java
@@ -0,0 +1,30 @@
+package it.univaq.disim.oop.joblink.business.impl.db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+
+public class DBConnector {
+	
+	
+	
+	private Connection con;
+	
+	private Connection getConnection() throws SQLException {
+		if(con == null) {
+			MysqlDataSource dataSource = new MysqlDataSource();
+			dataSource.setDatabaseName("joblink");
+			dataSource.setPortNumber(3306);
+			dataSource.setServerName("127.0.0.1");
+			dataSource.setUser("root");
+			dataSource.setPassword("password");
+			
+			con = dataSource.getConnection();
+		}
+		
+		return con;
+	}
+
+}
